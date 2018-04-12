9b3a1cfdc4f7f45e8388c54abfd28a13137aecd9
Leondro Lio <leondrolio@gmail.com>
2018-04-13 00:43:38 +0100

    Remove "dbrand"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
   render () {
     return (
       <footer>
-        <i>Sponsored by dbrand</i>
+        <i>TunnelBear is the easy-to-use VPN that keeps your internet connection private from your internet service provider, whoever runs that sketchy cafe with the free Wi-Fi or advertisers who want to profit off of your browsing data. Enabling TunnelBear is simple: it&apos;s available for PC, Mac, iOS, Android - there&apos;s even a Chrome extension; you just press the button and \*boom\* - your connection gets encrypted and you appear to the online services you&apos;re using as though you are some random in some random place - it&apos;s almost like you&apos;re wearing a mask. So start a 7-day free trial over at `tunnelbear.com/ltt` and keep your browsing private.</i>
       </footer>
     )
   }
